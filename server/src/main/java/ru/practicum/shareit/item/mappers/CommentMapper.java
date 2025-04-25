74680f94cf83d52067bcd36cc01f29b573484f79
Desidiam <desidiam@gmail.com>
2025-04-25 20:12:50 +0300

    Попытка обновить время в последний момент

--- server/src/main/java/ru/practicum/shareit/item/mappers/CommentMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mappers/CommentMapper.java
@@ -5,11 +5,14 @@ import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.model.Comment;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentMapper {
 
     public static CommentDto toCommentDto(Comment comment) {
-        return new CommentDto(comment.getId(), comment.getText(), comment.getUser().getName(), comment.getCreated());
+        return new CommentDto(comment.getId(), comment.getText(), comment.getUser().getName(), Timestamp.valueOf(LocalDateTime.now()));
     }
 
 }
