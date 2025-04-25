7e3d73b5c7bff2186c7e64188e3239f73faf80e3
Desidiam <desidiam@gmail.com>
2025-04-25 14:33:08 +0300

    Исправлен тест

--- server/src/test/java/ShareItServerTests.java
+++ server/src/test/java/ShareItServerTests.java
@@ -1,7 +1,6 @@
-package ru.practicum.shareit;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.ShareItServer;
 
 @SpringBootTest(classes = ShareItServer.class)
 public class ShareItServerTests {
